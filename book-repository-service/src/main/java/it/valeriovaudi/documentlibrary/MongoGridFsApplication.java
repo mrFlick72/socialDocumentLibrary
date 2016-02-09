c5b2a9f5900d49e534ced35bb54757606f418f4d
Valerio Vaudi <valerio.vaudi@gmail.com>
2016-02-09 23:15:54 +0100

    standard Servlet uplad File

--- book-repository-service/src/main/java/it/valeriovaudi/documentlibrary/MongoGridFsApplication.java
+++ book-repository-service/src/main/java/it/valeriovaudi/documentlibrary/MongoGridFsApplication.java
@@ -3,6 +3,7 @@ package it.valeriovaudi.documentlibrary;
 import org.springframework.beans.factory.config.PlaceholderConfigurerSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -11,6 +12,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.web.multipart.support.MultipartFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJms
@@ -22,6 +27,14 @@ import org.springframework.jms.annotation.EnableJms;
 public class MongoGridFsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MongoGridFsApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean multipartFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new MultipartFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 
     @Bean
