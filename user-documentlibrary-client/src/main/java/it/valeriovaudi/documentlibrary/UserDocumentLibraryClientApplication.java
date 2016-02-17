af85082de2fbe9d252c6e5aaaac49e5f700ce53d
Valerio Vaudi <valerio.vaudi@gmail.com>
2016-02-17 22:17:42 +0100

    common filter for response compression

--- user-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/UserDocumentLibraryClientApplication.java
+++ user-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/UserDocumentLibraryClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "it.valeriovaudi.documentlibrary.repository")
 @EnableTransactionManagement
 @PropertySource("classpath:restBaseUrl.properties")
+@ServletComponentScan(basePackages = "it.valeriovaudi.documentlibrary")
 public class UserDocumentLibraryClientApplication {
 
     public static void main(String[] args) {
@@ -29,5 +31,4 @@ public class UserDocumentLibraryClientApplication {
     public EmbeddedServletContainerCustomizer exceptionHandling() {
         return container -> container.addErrorPages(new ErrorPage("/exception"));
     }
-
 }
