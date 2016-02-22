2658f9bf8afb7a8e1b7bb18ea2abd8d671986329
Valerio Vaudi <valerio.vaudi@gmail.com>
2016-02-22 23:28:07 +0100

    profiling standAlone and cloud

--- user-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/UserDocumentLibraryClientApplication.java
+++ user-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/UserDocumentLibraryClientApplication.java
@@ -1,8 +1,5 @@
 package it.valeriovaudi.documentlibrary;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,11 +7,8 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreakerImportSelector;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClientImportSelector;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClientConfigurationRegistrar;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -22,12 +16,7 @@ import org.springframework.cloud.netflix.zuul.ZuulProxyConfiguration;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "it.valeriovaudi.documentlibrary.repository")
