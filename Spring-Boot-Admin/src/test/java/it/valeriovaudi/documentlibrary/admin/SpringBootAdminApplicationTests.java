06fd33673b94c259f000d09035e7a6488923f692
Valerio Vaudi <valerio.vaudi@gmail.com>
2016-03-06 22:45:58 +0100

    Spring boot admin server

---
+++ Spring-Boot-Admin/src/test/java/it/valeriovaudi/documentlibrary/admin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,18 @@
+package it.valeriovaudi.documentlibrary.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootAdminApplication.class)
+@WebAppConfiguration
+public class SpringBootAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
