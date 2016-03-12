2f4b04f59b06d53aa0fc4037f0c60b5a59c7f39c
Valerio Vaudi <valerio.vaudi@gmail.com>
2016-03-12 01:59:22 +0100

    active mq on docker

--- admin-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/AdminDocumentlibraryClientApplication.java
+++ admin-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/AdminDocumentlibraryClientApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -16,20 +15,27 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @SpringBootApplication
 @PropertySource("classpath:restBaseUrl.properties")
 @EnableJms
 @EnableIntegration
 @EnableWebSocketMessageBroker
+@EnableWebSocket
 @EnableJpaRepositories(basePackages = "it.valeriovaudi.documentlibrary.notify.repository")
 @EnableTransactionManagement
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableZuulProxy
 @RibbonClients
+@EnableAsync
 @EnableAspectJAutoProxy(proxyTargetClass = true) // without this declaration the RestTemplate injection will be fails becouse spring cloud proxied this class for load balance with netflix ribbon
 public class AdminDocumentlibraryClientApplication {
 
--- admin-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/config/WebSocketConfig.java
+++ admin-documentlibrary-client/src/main/java/it/valeriovaudi/documentlibrary/config/WebSocketConfig.java
@@ -1,20 +1,35 @@
 package it.valeriovaudi.documentlibrary.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.broker.AbstractBrokerMessageHandler;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompBrokerRelayMessageHandler;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.messaging.SubProtocolWebSocketHandler;
 
 /**
  * Created by Valerio on 10/07/2015.
  */
 
 @Configuration
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@EnableConfigurationProperties(ActiveMQProperties.class)
+public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport {
 
+
+    // todo externalize the web socker configuration properties
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/notify");
+        config.enableStompBrokerRelay("/notify").setRelayHost("192.168.99.100")
+                .setClientLogin("admin").setClientPasscode("admin")
+        .setSystemLogin("admin").setSystemPasscode("admin");
         config.setApplicationDestinationPrefixes("/app");
     }
 
